610e281748241d2df09779c9c7354757ce3560e6
kelvin-daniel <kaymutor@gmail.com>
2020-07-18 12:01:58 +0300

    add java script

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
+function animatedForm(){
+    const arrows =document.querySelectorAll(".fa-arrow-down");
+    arrows.forEach(arrow=> {
+      arrow.addEventListener('click', () => {
+          const input= arrow.previousElementSibling;
+          const parent = arrow.parentElement;
+          const nextForm= parent.nextElementSibling;
+      });
+    });
 
+}
+
+
+
+
+
+animatedForm();
